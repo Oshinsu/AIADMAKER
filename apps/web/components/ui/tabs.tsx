85b6f4adfbf7a7e9a4ddf52c0210b3bd1970496a
Gary Bissol <garybyss972@gmail.com>
2025-10-20 00:51:53 -0400

    Refactor TabsTrigger component by removing the 'value' prop
    - Removed the required 'value' prop from the TabsTrigger component to simplify its interface
    - Maintained compatibility with TabsPrimitive.Trigger while enhancing flexibility

--- apps/web/components/ui/tabs.tsx
+++ apps/web/components/ui/tabs.tsx
@@ -24,9 +24,7 @@ TabsList.displayName = TabsPrimitive.List.displayName
 
 const TabsTrigger = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger> & {
-    value: string
-  }
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
 >(({ className, ...props }, ref) => (
   <TabsPrimitive.Trigger
     ref={ref}
